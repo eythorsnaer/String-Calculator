50c70290b1e8f954a1b5b233dd00b3b65af89c7a
Eyþór Snær Tryggvason <eythorsntr@Eyors-MacBook-Pro.local>
2015-10-17 22:02:07 +0000

    restart

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,13 @@
+package is.ru.stringcalculator;
+
+public class Calculator { 
+	public static int add(String text){
+		if(text == "") {
+			return 0;
+		}
+		else {
+			return Integer.parseInt(text);
+		}
+
+	}
+}
---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,21 @@
+package is.ru.stringcalculator;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
+	}
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, Calculator.add(""));
+	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+		assertEquals(15, Calculator.add("15"));
+	}
+}
